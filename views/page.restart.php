f08ae7ab18806392544cb77cd2d814d39112532c
Michael Newton <miken32@gmail.com>
2019-12-12 15:39:29 -0700

    whitespace: fix indent

--- views/page.restart.php
+++ views/page.restart.php
@@ -30,106 +30,106 @@
 
 							<div class="tab-content display">
 								<div role="tabpanel" id="restartform" class="tab-pane active">
-							<!--Device List-->
-							<div class="element-container">
-								<div class="row">
-									<div class="col-md-12">
+									<!--Device List-->
+									<div class="element-container">
 										<div class="row">
-											<div class="form-group">
-												<div class="col-md-3">
-													<label class="control-label" for="xtnlist"><?= htmlspecialchars(_("Device List")) ?></label>
-													<i class="fa fa-question-circle fpbx-help-icon" data-for="xtnlist"></i>
-												</div>
-												<div class="col-md-9">
-													<div class="input-group">
-														<select class="form-control" id="xtnlist" multiple="multiple" name="restartlist[]" size="8">
+											<div class="col-md-12">
+												<div class="row">
+													<div class="form-group">
+														<div class="col-md-3">
+															<label class="control-label" for="xtnlist"><?= htmlspecialchars(_("Device List")) ?></label>
+															<i class="fa fa-question-circle fpbx-help-icon" data-for="xtnlist"></i>
+														</div>
+														<div class="col-md-9">
+															<div class="input-group">
+																<select class="form-control" id="xtnlist" multiple="multiple" name="restartlist[]" size="8">
 <?php foreach ($device_list as $device): ?>
-															<option value="<?= htmlspecialchars($device["id"]) ?>">
-																<?= htmlspecialchars("$device[id] - $device[description] - $device[ua] Device") ?>
-															</option>
+																	<option value="<?= htmlspecialchars($device["id"]) ?>">
+																		<?= htmlspecialchars("$device[id] - $device[description] - $device[ua] Device") ?>
+																	</option>
 <?php endforeach ?>
-														</select>
-														<span class="input-group-addon">
-															<button class="btn" id="selectall"><?= htmlspecialchars(_('SELECT ALL')) ?></button>
-														</span>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-md-12">
-										<span id="xtnlist-help" class="help-block fpbx-help-block">
-											<?= htmlspecialchars(_("Select Device(s) to restart.  Currently, only Aastra, Snom, Polycom, Grandstream and Cisco devices are supported.  All other devices will not show up in this list.  Click the \"Select All\" button to restart all supported devices.")) ?>
-										</span>
-									</div>
-								</div>
-							</div>
-							<!--END Device List-->
-							<!--Schedule Time-->
-							<div class="element-container">
-								<div class="row">
-									<div class="col-md-12">
-										<div class="row">
-											<div class="form-group">
-												<div class="col-md-3">
-													<label class="control-label" for="enable_schedule_n"><?= htmlspecialchars(_("Scheduled Reboot")) ?></label>
-													<i class="fa fa-question-circle fpbx-help-icon" data-for="schedtime"></i>
-												</div>
-												<div class="col-md-9 radioset">
-													<input type="radio" id="enable_schedule_n" name="enable_schedule" value="0" checked="checked"/>
-													<label for="enable_schedule_n"><?= htmlspecialchars(_("Now")) ?></label>
-													<input type="radio" id="enable_schedule_y" name="enable_schedule" value="1"/>
-													<label for="enable_schedule_y"><?= htmlspecialchars(_("Later")) ?></label>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-md-12">
-										<span id="enable_schedule-help" class="help-block fpbx-help-block">
-											<?= htmlspecialchars(_("You can reboot the devices now, or at a scheduled time in the next 24 hours.")) ?>
-										</span>
-									</div>
-								</div>
-							</div>
-							<!--END Schedule Time-->
-							<!--Schedule Time-->
-							<div class="element-container">
-								<div class="row">
-									<div class="col-md-12">
-										<div class="row">
-											<div class="form-group">
-												<div class="col-md-3">
-													<label class="control-label" for="schedtime"><?= htmlspecialchars(_("Reboot Time")) ?></label>
-													<i class="fa fa-question-circle fpbx-help-icon" data-for="schedtime"></i>
-												</div>
-												<div class="col-md-9">
-													<div class="input-group">
-														<input type="time" class="form-control" name="schedtime" id="schedtime" value="00:00" disabled="disabled"/>
-														<div class="input-group-addon">
-															<?= htmlspecialchars(_("Server time:")) ?>
-															<span id="idTime" data-time="<?= time() ?>" data-zone="<?= htmlspecialchars(date_default_timezone_get()) ?>">
-																<?= htmlspecialchars((new \DateTime)->format("H:i:s T")) ?>
-															</span>
+																</select>
+																<span class="input-group-addon">
+																	<button class="btn" id="selectall"><?= htmlspecialchars(_("SELECT ALL")) ?></button>
+																</span>
+															</div>
 														</div>
 													</div>
 												</div>
 											</div>
 										</div>
+										<div class="row">
+											<div class="col-md-12">
+												<span id="xtnlist-help" class="help-block fpbx-help-block">
+													<?= htmlspecialchars(_("Select Device(s) to restart.  Currently, only Aastra, Snom, Polycom, Grandstream and Cisco devices are supported.  All other devices will not show up in this list.  Click the \"Select All\" button to restart all supported devices.")) ?>
+												</span>
+											</div>
+										</div>
 									</div>
-								</div>
-								<div class="row">
-									<div class="col-md-12">
-										<span id="schedtime-help" class="help-block fpbx-help-block">
-											<?= htmlspecialchars(_("Select the time you wish the device(s) to reboot.")) ?>
-										</span>
+									<!--END Device List-->
+									<!--Schedule Enable-->
+									<div class="element-container">
+										<div class="row">
+											<div class="col-md-12">
+												<div class="row">
+													<div class="form-group">
+														<div class="col-md-3">
+															<label class="control-label" for="enable_schedule_n"><?= htmlspecialchars(_("Scheduled Reboot")) ?></label>
+															<i class="fa fa-question-circle fpbx-help-icon" data-for="schedtime"></i>
+														</div>
+														<div class="col-md-9 radioset">
+															<input type="radio" id="enable_schedule_n" name="enable_schedule" value="0" checked="checked"/>
+															<label for="enable_schedule_n"><?= htmlspecialchars(_("Now")) ?></label>
+															<input type="radio" id="enable_schedule_y" name="enable_schedule" value="1"/>
+															<label for="enable_schedule_y"><?= htmlspecialchars(_("Later")) ?></label>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-12">
+												<span id="enable_schedule-help" class="help-block fpbx-help-block">
+													<?= htmlspecialchars(_("You can reboot the devices now, or at a scheduled time in the next 24 hours.")) ?>
+												</span>
+											</div>
+										</div>
 									</div>
-								</div>
-							</div>
-							<!--END Schedule Time-->
+									<!--END Schedule Enable-->
+									<!--Schedule Time-->
+									<div class="element-container">
+										<div class="row">
+											<div class="col-md-12">
+												<div class="row">
+													<div class="form-group">
+														<div class="col-md-3">
+															<label class="control-label" for="schedtime"><?= htmlspecialchars(_("Reboot Time")) ?></label>
+															<i class="fa fa-question-circle fpbx-help-icon" data-for="schedtime"></i>
+														</div>
+														<div class="col-md-9">
+															<div class="input-group">
+																<input type="time" class="form-control" name="schedtime" id="schedtime" value="00:00" disabled="disabled"/>
+																<div class="input-group-addon">
+																	<?= htmlspecialchars(_("Server time:")) ?>
+																	<span id="idTime" data-time="<?= time() ?>" data-zone="<?= htmlspecialchars(date_default_timezone_get()) ?>">
+																		<?= htmlspecialchars((new \DateTime)->format("H:i:s T")) ?>
+																	</span>
+																</div>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										<div class="row">
+											<div class="col-md-12">
+												<span id="schedtime-help" class="help-block fpbx-help-block">
+													<?= htmlspecialchars(_("Select the time you wish the device(s) to reboot.")) ?>
+												</span>
+											</div>
+										</div>
+									</div>
+									<!--END Schedule Time-->
 								</div>
 								<div role="tabpanel" id="restartlist" class="tab-pane">
 									<table id="pending_restart_grid"
