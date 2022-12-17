0cf3d99822e2f0f7cc2300b875222cfd79230551
Michael Newton <miken32@gmail.com>
2022-12-16 21:13:48 -0700

    was there a reason I didn't do this before?

--- Job.php
+++ Job.php
@@ -11,22 +11,31 @@ class Job implements TaskInterface {
     public static function run(InputInterface $input, OutputInterface $output) {
         $output->writeln("");
         if ($input->getOption("force")) {
+            $jobname = "";
             $id = $input->getOption("run");
             $job = FreePBX::Job();
-            foreach ($job->getAll() as $row) {
+            foreach ($job->getAllEnabled() as $row) {
                 if ($row["id"] === $id) {
                     $jobname = $row["jobname"];
                     break;
                 }
             }
-            $output->writeln(sprintf(
-                _("Cannot force this job. Try \"fwconsole phonerestart --jobname %s\" instead."),
-                $jobname
-            ));
-            return false;
+            if (empty($jobname)) {
+                $output->writeln(_("Cannot find this job ID. Use \"fwconsole job --list\" to get the job ID (not name)."));
+                return false;
+            }
+            try {
+                FreePBX::Restart()->runJob($output, $jobname, strpos($jobname, "recurring") === false);
+            } catch (Exception $e) {
+                $output->writeln(sprintf(
+                    _("Cannot force this job. Try \"fwconsole phonerestart --jobname %s\" instead."),
+                    $jobname
+                ));
+                return false;
+            }
         }
         $output->writeln(_("Starting phone restarts..."));
-        \FreePBX::Restart()->runJobs($output);
+        FreePBX::Restart()->runJobs($output);
         $output->writeln(_("Finished"));
         return true;
     }
